f6a602427d8f9e1d9d9c9e1a68701d745363d212
Mahesh Chavda <chavdamahesh2772@gmail.com>
2025-03-07 23:16:30 +0530

    PWA support added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,85 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: ["**/*.{js,css,html,png,svg,ico,jpg,json}"],
+      },
+      includeAssets: [
+        "favicon.ico",
+        "images/favicon-16x16.png",
+        "images/favicon-32x32.png",
+        "images/logo.jpg",
+      ],
+      manifest: {
+        name: "Area Converter",
+        short_name: "AreaConverter",
+        description: "Easily convert area units with our intuitive tool.",
+        theme_color: "#ff69b4",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/images/maskable_icon_x48.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/maskable_icon_x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/maskable_icon_x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/maskable_icon_x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/maskable_icon_x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
